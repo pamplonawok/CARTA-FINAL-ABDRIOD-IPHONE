6f976e420e9e60b6985747fb28fe1015c09b9974
pamplonawok <zharanue@hotmail.com>
2026-08-12 00:55:46 +0200

    Update CACHE_NAME to new version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mrwok-v5.8-slider12';
+const CACHE_NAME = 'mrwok-v5.8-slider11-14';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', event => {
